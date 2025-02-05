3d6172ca105e3d76319a33ddd25fc4e5073af864
aleksakrsmanovic <aleksa.krsmanovic@ezdiharfintech.com>
2025-02-05 14:42:55 +0100

    default load balancing

--- serviceb/src/main/java/com/example/serviceb/config/ConfigClass.java
+++ serviceb/src/main/java/com/example/serviceb/config/ConfigClass.java
@@ -1,6 +1,8 @@
 package com.example.serviceb.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -14,10 +16,21 @@ public class ConfigClass {
 //        return RestClient.create();
 //    }
 
+    //round-robin default load balancer
     @LoadBalanced
     @Bean
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
+
+//    @Bean
+//    public ServiceInstanceListSupplier discoveryClientServiceInstanceListSupplier(
+//        ConfigurableApplicationContext context) {
+//        return ServiceInstanceListSupplier.builder()
+//            .withDiscoveryClient()
+//            .withWeighted()
+//            .withCaching()
+//            .build(context);
+//    }
 
 }
